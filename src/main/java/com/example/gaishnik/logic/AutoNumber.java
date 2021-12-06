735ce8d1aa114d0d30e552fd26ccce9256f44f4c
MisterMisix <95502909+MisterMisix@users.noreply.github.com>
2021-12-07 02:28:14 +0300

    Add files via upload

--- src/main/java/com/example/gaishnik/logic/AutoNumber.java
+++ src/main/java/com/example/gaishnik/logic/AutoNumber.java
@@ -5,16 +5,26 @@ import java.util.TreeSet;
 
 public class AutoNumber {
 
-    TreeSet<String> numbers = new TreeSet<>();
+    /* Очень хотелось добавить, но в по условию не требуется
+           прописать проверку на существующие регионы и готово,
+           можно даже CRUD "прикрутить" с возможность менять регион,
+           добавлять конкретные номера, удалять их и т.д...
+    public AutoNumber(int region) {
+        this.region = String.format(" %d RUS", region);
+    }
+       Ещё была мысль использовать паттерн проектирования "Singleton", но в контексте данного задания смысла не увидел,
+           точнее он теряется, если расскомментить конструктор с параметром.
+     */
+    TreeSet<String> numbers = new TreeSet<>(); //Т.к. всё равно планировал импорт TreeSet.
     String[] alphabeticCharacter = {"А", "В", "Е", "К", "М", "Н", "О", "Р", "С", "Т", "У", "Х"};
     String region = " 116 RUS";
-    int countSeriesActual = 0;
-    int number = 0;
-    String[] series = getSeries();
-    Random random = new Random();
+    int countSeriesActual = 0; //счётчик серии номеров.
+    int number = 0; //int часть номера.
+    String[] series = getSeries(); //все возможные сочетания допустимых букв.
+    Random random = new Random(); //Возможно было бы лучше использовать Math.random(), исправить - 1 минута.
 
     private String[] getSeries() {
-        TreeSet<String> series = new TreeSet<>();
+        TreeSet<String> series = new TreeSet<>(); //Выбор пал на TreeSet для порядка и отсутствия дубликатов.
         for (String s : alphabeticCharacter) {
             for (String value : alphabeticCharacter) {
                 for (String item : alphabeticCharacter) {
@@ -27,7 +37,7 @@ public class AutoNumber {
                 }
             }
         }
-        return series.toArray(new String[0]);
+        return series.toArray(new String[0]);//для удобства, всё равно весь метод отработает всего один раз.
     }
 
     private String generateNext() {
@@ -35,11 +45,10 @@ public class AutoNumber {
         if (numbers.isEmpty()) {
             this.number = 0;
         }
-        if (number == 1000) {
+        if (number == 1000) { //1000 номеров в серии (от 000 до 999).
             this.number = 0;
-            if (countSeriesActual == 1727) {
+            if (countSeriesActual == 1727) { //1728, начиная с 0. Если номер например Х999ХХ - next будет А000АА, если ещё не был и т.д.
                 this.countSeriesActual = 0;
-
             } else {
                 this.countSeriesActual++;
             }
@@ -50,11 +59,11 @@ public class AutoNumber {
     }
 
     public String getNext() {
-        if (numbers.size() == 1_728_000) {
+        if (numbers.size() == 1_728_000) { //больше нет уникальных комбинаций.
             return "All numbers are busy";
         }
         String str = generateNext();
-        while (numbers.contains(str)) {
+        while (numbers.contains(str)) { // генерация следующего, если текущий уже был.
             str = generateNext();
         }
         this.numbers.add(str);
@@ -63,12 +72,12 @@ public class AutoNumber {
 
 
     private String generateRandom() {
-        this.number = random.nextInt(1000);
-        this.countSeriesActual = random.nextInt(1728);
+        this.number = random.nextInt(1000); //"рандомная" int часть.
+        this.countSeriesActual = random.nextInt(1728);//"рандоминая" серия.
         return String.format("%s%03d%2s", series[countSeriesActual].charAt(0), number, series[countSeriesActual].substring(1, 3) + region);
     }
 
-    public String getRandom() {
+    public String getRandom() { //аналогично getNext();
         if (numbers.size() == 1_728_000) {
             return "All numbers are busy";
         }
